91ff25bdfdc04a941ab502fdf48b8b19698745dc
SonNguyen <son15052002@gmail.com>
2023-11-15 10:57:20 +0700

    update: update FormatUtils adjust currency

--- IAP/src/main/java/com/mct/iap/banner/utils/FormatUtils.java
+++ IAP/src/main/java/com/mct/iap/banner/utils/FormatUtils.java
@@ -2,6 +2,7 @@ package com.mct.iap.banner.utils;
 
 import androidx.annotation.NonNull;
 
+import java.math.RoundingMode;
 import java.text.NumberFormat;
 import java.util.Currency;
 
@@ -18,12 +19,33 @@ public class FormatUtils {
             return "";
         }
         try {
+            Currency currency = Currency.getInstance(priceCurrencyCode);
             NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
-            numberFormat.setCurrency(Currency.getInstance(priceCurrencyCode));
-            numberFormat.setMinimumFractionDigits(value % 1 != 0 ? 2 : 0);
+            numberFormat.setRoundingMode(RoundingMode.HALF_EVEN);
+            numberFormat.setCurrency(currency);
+            adjustDefaultFractionDigits(numberFormat, currency);
             return numberFormat.format(value);
         } catch (Exception e) {
             return "";
+        }
+    }
+
+    private static void adjustDefaultFractionDigits(@NonNull NumberFormat numberFormat, Currency defaultCurrency) {
+        Currency currency = numberFormat.getCurrency() == null ? defaultCurrency : numberFormat.getCurrency();
+        if (currency != null) {
+            int digits = currency.getDefaultFractionDigits();
+            if (digits != -1) {
+                int oldMinDigits = numberFormat.getMinimumFractionDigits();
+                // Common patterns are "#.##", "#.00", "#".
+                // Try to adjust all of them in a reasonable way.
+                if (oldMinDigits == numberFormat.getMaximumFractionDigits()) {
+                    numberFormat.setMinimumFractionDigits(digits);
+                    numberFormat.setMaximumFractionDigits(digits);
+                } else {
+                    numberFormat.setMinimumFractionDigits(Math.min(digits, oldMinDigits));
+                    numberFormat.setMaximumFractionDigits(digits);
+                }
+            }
         }
     }
 
