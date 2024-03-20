09da5586b83490b3216265d85a97c62c14438d04
SonNguyen <son15052002@gmail.com>
2024-03-20 08:29:43 +0700

    update: formatMoney

--- IAP/src/main/java/com/mct/iap/banner/utils/FormatUtils.java
+++ IAP/src/main/java/com/mct/iap/banner/utils/FormatUtils.java
@@ -5,22 +5,28 @@ import androidx.annotation.NonNull;
 import java.math.RoundingMode;
 import java.text.NumberFormat;
 import java.util.Currency;
+import java.util.Locale;
 
 public class FormatUtils {
 
     @NonNull
     public static String formatTime(int value) {
-        return value < 10 ? "0" + value : "" + value;
+        return value < 10 ? "0" + value : String.valueOf(value);
     }
 
     @NonNull
     public static String formatMoney(float value, String priceCurrencyCode) {
+        return formatMoney(value, priceCurrencyCode, findLocale(priceCurrencyCode, Locale.getDefault(Locale.Category.FORMAT)));
+    }
+
+    @NonNull
+    public static String formatMoney(float value, String priceCurrencyCode, Locale locale) {
         if (value < 0) {
             return "";
         }
         try {
             Currency currency = Currency.getInstance(priceCurrencyCode);
-            NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+            NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale);
             numberFormat.setRoundingMode(RoundingMode.HALF_EVEN);
             numberFormat.setCurrency(currency);
             adjustDefaultFractionDigits(numberFormat, currency);
@@ -28,6 +34,23 @@ public class FormatUtils {
         } catch (Exception e) {
             return "";
         }
+    }
+
+    private static Locale findLocale(String priceCurrencyCode, Locale fallback) {
+        Currency c = Currency.getInstance(priceCurrencyCode);
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+        numberFormat.setCurrency(c);
+        String formattedValue = numberFormat.format(1).replaceAll("[\\d\\s.,]", "");
+        for (Locale locale : Locale.getAvailableLocales()) {
+            try {
+                Currency currency = Currency.getInstance(locale);
+                if (formattedValue.contains(currency.getSymbol())) {
+                    return locale;
+                }
+            } catch (Exception ignored) {
+            }
+        }
+        return fallback;
     }
 
     private static void adjustDefaultFractionDigits(@NonNull NumberFormat numberFormat, Currency defaultCurrency) {
